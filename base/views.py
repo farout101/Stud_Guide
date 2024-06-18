79f080d9078479408024d2119ddbbbd9b99ace68
Phyo Zaw Linn <phyozawlinn1852020@gmail.com>
2024-06-18 11:21:29 +0630

    blue print

--- base/views.py
+++ base/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from base.models import Room
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    rooms = Room.objects.all()
+    context = {
+        'rooms': rooms
+    }
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request,pk):
+    room = Room.objects.get(id=pk)
+    context = {
+        'room': room
+    }
+    return render(request, 'base/room.html', context)
