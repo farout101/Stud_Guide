f515467d1e0dbf1ae05023497d406a3eacfeca6d
Phyo Zaw Linn <phyozawlinn1852020@gmail.com>
2024-06-19 21:42:11 +0630

    components

--- base/views.py
+++ base/views.py
@@ -68,7 +68,7 @@ def home(request):
         )
     room_count = rooms.count()
     topics = Topic.objects.all()
-    room_messages = Message.objects.all() 
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains = q))
     context = {
         'rooms': rooms,
         'topics': topics,
